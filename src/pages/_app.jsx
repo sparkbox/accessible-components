97cb81cf97f1d3953ab5febdfe114215c019c41f
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-10-06 10:21:59 -0500

    feat: Added html structure
    - Added html elements.
    - Added text to paragraph.
    
    FSA22V2-220

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/globals.scss';
+import '../styles/components/index.scss';
 
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,25 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <main>
-        <h1>
-          Welcome to the Accessibility Capstone!
-        </h1>
-        <ul>
-          <li><Link href="/accordion">Accordion</Link></li>
-          <li><Link href="/dialog">Dialog</Link></li>
-          <li><Link href="/disclosure">Disclosure</Link></li>
-          <li><Link href="/tabs">Tabs</Link></li>
-        </ul>
+      <main className="home">
+        <div className="home-top">
+          <div>
+            <h1>Accessibility Cheatsheets for Components</h1>
+            <p className="home-top__top-description">
+              Intro copy goes here, such as: Build accessible components 
+              more accurately and in less time with these easy-to-read 
+              cheatsheets based on WCAG and ARIA best practices. 
+              Check back often as we’re adding new components frequently!
+            </p>
+            <ul>
+              <li><Link href="/accordion">Accordion</Link></li>
+              <li><Link href="/dialog">Dialog</Link></li>
+              <li><Link href="/disclosure">Disclosure</Link></li>
+              <li><Link href="/tabs">Tabs</Link></li>
+            </ul>
+          </div>
+        </div>
+        <img className="home-top__top-image" src="/home-header-image.svg" alt="Illustration of dog surrounded by accessible components" />
       </main>
     </div>
   );
