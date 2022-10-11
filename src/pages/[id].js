18ac2d92607f36b625216d85d7b456ec74718fab
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-10-11 10:50:17 -0500

    refactor: Removed unnecessary code
    - Deleted eslint rules because they were not good to use.
    - Deleted index.scss file because it didn't follow the ITCSS pattern.
    
    FSA22V2-212

--- src/pages/[id].js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function Details() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <h1>{id}</h1>;
-}
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,4 @@
 import '../styles/globals.scss';
-import '../styles/components/index.scss';
 
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
