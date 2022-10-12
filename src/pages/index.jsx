d423b9648290b931320b8120a73e8a5ec3a731b4
Terri Isidro <terri.isidro@heysparkbox.com>
2022-10-12 14:45:47 -0400

    fix: error on console
    -reinstall node modules and package-lock.json and re-committing to fix.
    
    FSA22V2-230

---
+++ src/pages/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout pageTitle="Home">
+      <div>
+        <div className="home-top">
+          <div>
+            <h1>Accessibility Cheatsheets for Components</h1>
+            <p className="home-top__top-description">
+              Intro copy goes here, such as: Build accessible components more
+              accurately and in less time with these easy-to-read cheatsheets
+              based on WCAG and ARIA best practices. Check back often as we’re
+              adding new components frequently!
+            </p>
+            <ul>
+              <li>
+                <Link href="/accordion">
+                  <a>Accordion</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dialog">
+                  <a>Dialog</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/disclosure">
+                  <a>Disclosure</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/tabs">
+                  <a>Tabs</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img
+          className="home-top__top-image"
+          src="/home-header-image.svg"
+          alt="Illustration of dog surrounded by accessible components"
+        />
+      </div>
+    </Layout>
+  );
+}
