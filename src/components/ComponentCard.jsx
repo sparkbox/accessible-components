99a142095c4c022603c68fb534057ad4cbbdbba2
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-10-13 15:51:42 -0400

    fix: insert missing a tags
    -After starting to keyboard test, I realized none of the Next links show
    up in the markup as anchor tags. That means they cannot be used to
    navigate to pages. Reinserting them here makes them keyboard accessible
    again!
    -Fixed SCSS linting
    -The alt text associated with the component card images isn't really
    valuable. Those illustrative examples are just decorative.
    -Instead of just checking whether or not the components could be visible,
    (for instance, having some property like display:none or visibility:
    hidden), changing the test to "toBeInTheDocument" should ensure the
    component cards actually made it to the HTML.
    
    FSA22V2-000

--- src/components/ComponentCard.jsx
+++ src/components/ComponentCard.jsx
@@ -3,17 +3,12 @@ import Link from 'next/link';
 export default function ComponentCard({ name, slug, image }) {
   return (
     <Link href={slug}>
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-      <div className="component-card" tabIndex={0}>
-        <span className="component-card__header">
-          {name}
-        </span>
-        <img
-          src={image}
-          alt={`example of ${name} element`}
-          className="component-card__image"
-        />
-      </div>
+      <a className="component-link">
+        <div className="component-card">
+          <span className="component-card__header">{name}</span>
+          <img src={image} alt="" className="component-card__image" />
+        </div>
+      </a>
     </Link>
   );
 }
