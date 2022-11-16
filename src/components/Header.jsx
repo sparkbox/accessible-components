9e83a757ede7368c137fb626d1116e19d8547673
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-11-16 10:05:35 -0500

    refactor: remove sparkbox logo
    -Remove branding and replace with a text header instead.
    -Add new styles for new header.
    -Delete unused sparkbox logo
    
    FSA22V2-285

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
-import Logo from './Logo';
 
 export default function Header() {
   return (
     <header className="cmp-header" data-testid="header">
       <Link href="/" aria-label="To home page" className="cmp-header__link">
-        <Logo />
+        Accessible Components
       </Link>
     </header>
   );
