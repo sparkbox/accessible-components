7ea6b14cc12e8ad0fe8228eb074d8d61f13311f5
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-11-22 10:08:23 -0500

    refactor: change component inventory
    Rename Component Inventory to Related Components
    
    FSA22V2-302

--- src/components/RelatedComponents.jsx
+++ src/components/RelatedComponents.jsx
@@ -1,7 +1,7 @@
 export default function RelatedComponents({ components }) {
   return (
     <div className="rel-component" data-testid="rel-component">
-      <h2 className="rel-component__heading">Component Inventory</h2>
+      <h2 className="rel-component__heading">Related Components</h2>
       <div className="rel-component__list">
         {components.map((cmt) => (
           <a
