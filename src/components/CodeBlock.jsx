d3b7863e660ed16820ee9789bc23b492fc862dff
Terri Isidro <terri.isidro@heysparkbox.com>
2022-11-29 13:51:34 -0500

    feat: dynamic name for class
    - added dynamic name to the class name in code block jsx file to improve
    functioning.
    
    - updated code block test to include testing for return of dynamic h2
    name rendering. Also refactored the test structure.
    
    FSA22V2-298

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,6 +1,6 @@
 export default function CodeBlock({ name, exportedCodeString }) {
   return (
-    <article className="cmp-code-block">
+    <article className={`cmp-code-block obj-code-block__${name.toLowerCase()}`}>
       <h2 className="cmp-code-block__heading">{name}</h2>
       <section className="cmp-code-block__content obj-code-block__child">
         {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
--- src/components/CodeBlock.test.jsx
+++ src/components/CodeBlock.test.jsx
@@ -4,16 +4,22 @@ import { render, screen } from '@testing-library/react';
 import CodeBlock from './CodeBlock';
 
 describe('CodeBlock', () => {
-  render(<CodeBlock exportedCodeString="<html returned>" />);
-  const returnedPreTag = screen.queryByTestId('pre-tag-found');
-  const returnedCodeTag = screen.queryByTestId('code-tag-found');
-  const returnedCodeBlock = screen.getByRole('code');
+  beforeEach(() => {
+    render(<CodeBlock exportedCodeString="<html returned>" name="html" />);
+  });
 
   it('returns pre & code tags', () => {
+    const returnedPreTag = screen.queryByTestId('pre-tag-found');
+    const returnedCodeTag = screen.queryByTestId('code-tag-found');
     expect(returnedPreTag).toBeInTheDocument();
     expect(returnedCodeTag).toBeInTheDocument();
   });
+  it('returns a string based on the name prop', () => {
+    const returnedName = screen.getByText('html');
+    expect(returnedName).toBeInTheDocument();
+  });
   it('returns a block of code based on the string given', () => {
+    const returnedCodeBlock = screen.getByRole('code');
     expect(returnedCodeBlock.innerHTML).toEqual('&lt;html returned&gt;');
   });
 });
