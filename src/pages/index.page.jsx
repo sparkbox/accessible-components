8028ee2095f130acc6bd7bb5843d6603f4f1d744
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2022-12-09 17:22:25 -0500

    refactor: svg/symbol/use
    WIP: experimenting with symbol and use tags.
    
    FSA22V2-000

--- src/pages/index.page.jsx
+++ src/pages/index.page.jsx
@@ -6,16 +6,16 @@ import ComponentCard from '../components/ComponentCard';
 export default function Home() {
   return (
     <Layout pageTitle="Home">
-      <svg class="svg-partials">
+      <svg xmlns="http://www.w3.org/2000/svg" class="svg-partials">
         <symbol id="doodle_arrow" viewBox="0 0 80 98.1">
           <path
             d="m80 48-2-34c-.3-4.5-1-9.6-4.6-12.2C70.3-.6 66-.2 62 .4L31.8 4.7c-3.2.5-4 5-2.5 7.7 1.6 2.8 4.5 4.5 6.6 6.8 2.1 2.4 3.3 6.3 1.1 8.6A324.2 324.2 0 0 0 1.2 73 5.3 5.3 0 0 0 0 76.2 5.4 5.4 0 0 0 1.5 79l13 16.2c1.2 1.5 2.8 3.2 4.7 2.9 1.3-.3 2.2-1.4 3-2.5l36-46.7c2.5-3.1 7.4-.6 10.4 2 3 2.7 7.8 5.4 10.3 2.3 1.1-1.4 1.1-3.4 1-5.3zM6.6 75.3c-.4-3.6 2-6.8 4.3-9.6l30.4-37.2c1-1.2 2.1-2.6 2-4.3a6.8 6.8 0 0 0-1.5-3.5L33.5 9q15.3-2.7 30.7-4.4l1.2 8.9c-.4 8.2 4.4 18 .4 25.7-.9-6-9.6-8.7-13.7-4.3Q32 56 14.4 78.8C12 81.7 7 79 6.7 75.4z"
             fill="currentColor"
           />
         </symbol>
-        <symbol id="doodle_circle" viewBox="0 0 139.6 59.8">
+        <symbol id="doodle_o" viewBox="0 0 139.6 59.8">
           <path
-            d="M17 54.7C8.5 51.3.4 44.5 0 35.5c-.3-7 4.1-13.4 9.2-18.2A68.9 68.9 0 0 1 48.1.7C62.3-1 76.7.7 90.8 3.5c13.4 2.7 27 6.6 38 14.7 6.9 5 13 13.4 10 21.2-2.3 6-8.7 9-14.7 11a230.3 230.3 0 0 1-63.8 9.1c-14.7.6-29.8.8-43.4-4.7M64.3 4.5a77.8 77.8 0 0 0-49.6 14.3c-5 3.6-10 8.5-10.2 14.7-.3 7 5.6 13 12 16A70.6 70.6 0 0 0 46.2 55a303.3 303.3 0 0 0 73.1-7.6c4.4-1 9-2.2 12.2-5.3 3.3-3 4.8-8.5 2-12a49.7 49.7 0 0 0-27.3-17.6c2.9 2.2 1.7 7.8-1.8 8.7.1-5-4-9.5-8.6-11.3-4.7-2-10-1.8-15-1.7l-24.5.6a9.5 9.5 0 0 1 8-4.3z"
+            d="M31.3 8a21 21 0 0 0-6.2-6c-5-3-11.5-2.3-16.3.8A20.8 20.8 0 0 0 .2 17c-1 7.2 1.8 15.5 8.4 18.6 4 2 8.9 1.6 13.1.3 7.3-2.3 14-8.6 14-16.3 0-4.2-2-8.2-4.4-11.6zM9.6 20c-.4-3.1.6-6.6 3.1-8.4 4.8-3.4 12.2 1.6 12 7.4a8.5 8.5 0 0 1-5 7.3 7.4 7.4 0 0 1-8.4-2A8.7 8.7 0 0 1 9.6 20z"
             fill="currentColor"
           />
         </symbol>
@@ -71,10 +71,15 @@ export default function Home() {
           </div>
         </div>
         <article className="components-grid">
-          <h2 className="components-grid__heading">Components</h2>
-          <svg class="doodle line">
-            <use href="doodle-line" />
-          </svg>
+          <h2 className="components-grid__heading">
+            Components
+            <svg
+              class="doodle doodle-line-pseudo-heading"
+              viewBox="0 0 131.7 139.7"
+            >
+              <use href="#doodle_line" />
+            </svg>
+          </h2>
           <ul className="components-grid__list">
             <li className="components-grid__item components-grid__item--accordion">
               <ComponentCard
@@ -89,8 +94,8 @@ export default function Home() {
                 slug="/dialog"
                 image="/images/components/dialog.svg"
               />
-              <svg class="doodle o">
-                <use href="doodle-o" />
+              <svg class="doodle doodle-o-pseudo" viewBox="0 0 139.6 59.8">
+                <use href="#doodle_o" />
               </svg>
             </li>
             <li className="components-grid__item components-grid__item--disclosure">
@@ -99,8 +104,11 @@ export default function Home() {
                 slug="/disclosure"
                 image="/images/components/disclosure.svg"
               />
-              <svg class="doodle diamond">
-                <use href="doodle-diamond" />
+              <svg
+                class="doodle doodle-diamond-pseudo-grid"
+                viewBox="0 0 50.6 76.8"
+              >
+                <use href="#doodle_diamond" />
               </svg>
             </li>
             <li className="components-grid__item components-grid__item--tabs">
@@ -109,25 +117,31 @@ export default function Home() {
                 slug="/tabs"
                 image="/images/components/tabs.svg"
               />
-              <svg class="doodle x">
-                <use href="doodle-x" />
+              <svg class="doodle doodle-x__pseudo" viewBox="0 0 49.3 54.8">
+                <use href="#doodle_x" />
               </svg>
             </li>
             <li className="components-grid__item components-grid__item--max-scott">
               <div className="max-wrapper">
-                <svg class="doodle arrow">
-                  <use href="doodle-arrow" />
+                <svg class="doodle doodle-arrow__pseudo" viewBox="0 0 80 98.1">
+                  <use href="#doodle_arrow" />
                 </svg>
-                <svg class="doodle diamond">
-                  <use href="doodle-diamond" />
+                <svg
+                  class="doodle doodle-diamond__pseudo--max"
+                  viewBox="0 0 50.6 76.8"
+                >
+                  <use href="#doodle_diamond" />
+                </svg>
+                <svg
+                  class="doodle doodle-line-pseudo-max"
+                  viewBox="0 0 131.7 139.7"
+                >
+                  <use href="#doodle_line" />
                 </svg>
                 <img
                   src="/images/max-grid.svg"
                   alt="Illustration of cute small white terrier dog with tongue out and head turned left looking like he is curious"
                 />
-                <svg class="doodle line">
-                  <use href="doodle-line" />
-                </svg>
               </div>
             </li>
           </ul>
