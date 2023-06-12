d71a8a58faddd9d935796142de900ab704247b31
Melissa Thompson <mthompson@heysparkbox.com>
2023-06-12 14:49:24 -0400

    feat: Add Google Tag Manager
    SC-1719

--- src/pages/_document.page.jsx
+++ src/pages/_document.page.jsx
@@ -3,10 +3,29 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KQ53HCR');
+              `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <noscript
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ53HCR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
   );
